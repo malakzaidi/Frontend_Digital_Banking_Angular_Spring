c9d341a0757532218d1406f9ee1974b11f05fa28
Malak Zaidi <malakzaidi815@gmail.com>
2025-05-18 13:32:20 +0000

    specify the routes

--- ebanking_web/src/app/app.routes.ts
+++ ebanking_web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CustomersComponent} from './customers/customers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomersComponent
+  },
+
+];
